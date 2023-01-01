fe1a3c2f7d3434ad690713bae2dcc422bcfa0d07
christian <74213268+christian-pletbjerg@users.noreply.github.com>
2023-01-01 02:51:44 -0700

    Changed cabal to be aware of our system dependency sqlite library

--- flake.nix
+++ flake.nix
@@ -11,16 +11,20 @@
       pkgs = import nixpkgs { inherit system; };
       version = "0.1.0.0";
 
-
       # Create the project
       project = pkgs.haskellPackages.developPackage
         {
             root = ./.;
             name = "haskell-sqlite3";
             modifier = pkgs.haskell.lib.compose.overrideCabal (drv:
-                { librarySystemDepends = 
-                    [ sqlite3.packages.${system}.default ]
-                    ++ (drv.librarySystemDepends or []); 
+                {
+                    # Some awkwardness from abnormal default behavior from
+                    # `cabal2nix` automatically hard coding the conversion from
+                    # `sqlite3` to `sqlite`
+                    # See
+                    # [here](https://github.com/NixOS/cabal2nix/blob/021a48f4b4942462154b06fd81429a248638f87f/cabal2nix/src/Distribution/Nixpkgs/Haskell/FromCabal/Name.hs)
+                    librarySystemDepends = [ sqlite3.packages.${system}.default ];
+                    libraryPkgconfigDepends = [ sqlite3.packages.${system}.default ];
                 });
 
         };
